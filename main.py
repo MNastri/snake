0e4b66e3e8a1f8c9ce36b9d714a74cce94906f96
MNastri <marcellonastri@gmail.com>
2021-10-06 17:32:24 -0300

    Create window for the game

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pygame
+import time
+
+
+WIDTH = 600
+HEIGHT = 600
+
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+
 def main_loop():
-    pass
+    pygame.init()
+    pygame.display.set_caption('Snake')
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen.fill(WHITE)
+    pygame.display.flip()
+    time.sleep(3.0)
 
 
 if __name__ == "__main__":
