7ce4016f4ae78093db4d843e99a5b64a3cd4145a
MNastri <marcellonastri@gmail.com>
2021-10-08 11:53:38 -0300

    Create function to find an empty cell on the board

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import numpy as np
+import random
+
+
+BOARD_ROWS = 3
+BOARD_COLUMNS = 3
 
 
 class Board:
     def __init__(self):
-        self.board = np.zeros((10, 10))
-    pass
+        self.array = np.zeros((BOARD_ROWS, BOARD_COLUMNS), dtype=int)
+
+    def find_empty_cell(self) -> tuple[int, int]:
+        # find an empty cell and return its position as tuple of the form (row, column)
+        valid_cell = False
+        while not valid_cell:
+            row = random.randrange(BOARD_ROWS)
+            column = random.randrange(BOARD_COLUMNS)
+            valid_cell = self.array[row][column] == 0
+        return row, column
+
 
 def main_loop():
-    pass
+    random.seed(a=0)
+    board = Board()
+    rr, cc =board.find_empty_cell()
+    print(f'{rr},{cc}')
+    board.array[rr][cc] = 1
+    rr, cc = board.find_empty_cell()
+    print(f'{rr},{cc}')
 
 if __name__ == "__main__":
     main_loop()
