7317f7a2dc928ff64deebe1fecf4b9ad65efe0a6
MNastri <marcellonastri@gmail.com>
2021-10-08 10:42:28 -0300

    Prototype a board class to save game states

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main_loop():
+import numpy as np
+
+
+class Board:
+    def __init__(self):
+        self.board = np.zeros((10, 10))
     pass
 
+def main_loop():
+    pass
 
 if __name__ == "__main__":
     main_loop()
