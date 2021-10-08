02d50c320db3ecc4d85c28f618164a819510834b
MNastri <marcellonastri@gmail.com>
2021-10-08 17:01:57 -0300

    Add console board visualization and small fixes

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Board:
         self.array = np.zeros((BOARD_ROWS, BOARD_COLUMNS), dtype=int)
 
     def find_empty_cell(self) -> tuple[int, int]:
-        # find an empty cell and return its position as tuple of the form (row, column)
+        # find an empty cell and return its position as tuple
         valid_cell = False
         while not valid_cell:
             row = random.randrange(BOARD_ROWS)
@@ -23,11 +23,13 @@ class Board:
 def main_loop():
     random.seed(a=0)
     board = Board()
-    rr, cc =board.find_empty_cell()
-    print(f'{rr},{cc}')
-    board.array[rr][cc] = 1
-    rr, cc = board.find_empty_cell()
-    print(f'{rr},{cc}')
+    print(board.array)
+    for _ in range(BOARD_ROWS * BOARD_COLUMNS):
+        rr, cc = board.find_empty_cell()
+        print(f'{rr},{cc}')
+        board.array[rr][cc] = 1
+        print(board.array)
+
 
 if __name__ == "__main__":
     main_loop()
