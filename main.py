cd5dde45e1f74443d9f5930b2aa5543f8889deaa
MNastri <marcellonastri@gmail.com>
2021-10-09 18:53:22 -0300

    Add a counter for empty cells
    Since trying to find an empty cell can enter an infinite loop, it was decided
    that a counter would be needed.
    
    Implemented it in the other methods of the board class accordingly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from typing import Union
 
 
 BOARD_ROWS = 3
@@ -9,9 +10,12 @@ BOARD_COLUMNS = 3
 class Board:
     def __init__(self):
         self.array = np.zeros((BOARD_ROWS, BOARD_COLUMNS), dtype=int)
+        self.empty_cells = BOARD_ROWS * BOARD_COLUMNS
 
-    def find_empty_cell(self) -> tuple[int, int]:
+    def find_empty_cell(self) -> Union[tuple[int, int], None]:
         # find an empty cell and return its position as tuple
+        if not self.empty_cells:
+            return
         valid_cell = False
         row = column = None
         while not valid_cell:
@@ -21,6 +25,7 @@ class Board:
         return row, column
 
     def set_cell(self, value: int, row: int, column: int) -> None:
+        self.empty_cells -= 1 if self.array[row][column] == 0 else 0
         self.array[row][column] = value
 
 
